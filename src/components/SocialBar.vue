944ee7c5271ca51085cd8b7a87589f5f2f813764
fbytie <91059792+fbytie@users.noreply.github.com>
2021-10-06 16:31:10 +0300

    feedback patch

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -9,19 +9,11 @@
         :icon="link.name"
         :size="24"
       />
-      <!-- {{ link.name }} -->
-      <!--
-      <font-awesome
-        :icon="link.icon"
-        class="near-white h1"
-        alt="link.name"
-      />-->
     </a>
   </div>
 </template>
 
 <script>
-/* import { faCamera, faKiwiBird, faPaintBrush } from '@fortawesome/free-solid-svg-icons' */
 
 import FeatherIcon from './FeatherIcon'
 
@@ -36,19 +28,16 @@ export default {
         name: 'gift',
         key: 0,
         href: 'https://www.patreon.com/bytie'
-        /* icon: faPaintBrush */
       },
       {
         name: 'twitter',
         key: 1,
         href: 'https://twitter.com/fBytie'
-        /* icon: faKiwiBird */
       },
       {
         name: 'instagram',
         key: 2,
         href: 'https://www.instagram.com/fbytie/'
-        /* icon: faCamera */
       }]
     }
   }
--- src/main.js
+++ src/main.js
@@ -4,13 +4,6 @@ import router from './router'
 import store from './store'
 import 'tachyons'
 import './assets/style.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faCamera, faKiwiBird, faPaintBrush } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUserSecret, faCamera, faKiwiBird, faPaintBrush)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
